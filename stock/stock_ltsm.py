e69242704a1330ee268b49ed309c21309e06a3da
Shigeru Hayashi <hayashi@improid.net>
2017-12-24 17:33:02 +0900

    Modified some script files and add some stock price files.

--- stock/stock_ltsm.py
+++ stock/stock_ltsm.py
@@ -8,7 +8,7 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.layers.recurrent import LSTM
 
-class Prediction :
+class Prediction:
 
     def __init__(self):
         self.length_of_sequences = 10
@@ -24,7 +24,7 @@ class Prediction :
         retY = numpy.array(Y)
         return retX, retY
 
-    def create_model(self) :
+    def create_model(self):
         model = Sequential()
         model.add(LSTM(self.hidden_neurons, \
                        batch_input_shape=(None, self.length_of_sequences, self.in_out_neurons), \
@@ -34,20 +34,17 @@ class Prediction :
         model.compile(loss="mape", optimizer="adam")
         return model
 
-    def train(self, X_train, y_train) :
+    def train(self, X_train, y_train, epochs=100):
         model = self.create_model()
-        # 学習
-        model.fit(X_train, y_train, batch_size=10, nb_epoch=100)
+        model.fit(X_train, y_train, batch_size=100, epochs=epochs)
         return model
 
-if __name__ == "__main__":
-
+def _run(file_prefix, epochs):
     p = Prediction()
-    
     # データ準備
     data = None
     for year in range(2007, 2017):
-        data_ = pandas.read_csv('stock/csv/日経平均株価_日足_' + str(year) +  '.csv')
+        data_ = pandas.read_csv('csv/' + file_prefix + '_' + str(year) +  '.csv')
         data = data_ if (data is None) else pandas.concat([data, data_])
     data.columns = ['date', 'open', 'high', 'low', 'close']
     data['date'] = pandas.to_datetime(data['date'], format='%Y-%m-%d')
@@ -56,17 +53,22 @@ if __name__ == "__main__":
     data = data.sort_values(by='date')
     data = data.reset_index(drop=True)
     data = data.loc[:, ['date', 'close']]
-    
     # 2割をテストデータへ
     split_pos = int(len(data) * 0.8)
     x_train, y_train = p.load_data(data[['close']].iloc[0:split_pos], p.length_of_sequences)
     x_test,  y_test  = p.load_data(data[['close']].iloc[split_pos:], p.length_of_sequences)
-    
-    model = p.train(x_train, y_train)
-    
+    model = p.train(x_train, y_train, epochs)
     predicted = model.predict(x_test)
     result = pandas.DataFrame(predicted)
     result.columns = ['predict']
     result['actual'] = y_test
-    result.plot()
-    plt.show()
+    plot = result.plot(title='Stock prices prediction (epoch: {0})'.format(epochs))
+    fig = plot.get_figure()
+    fig.savefig('epoch_{0:03d}.png'.format(epochs))
+
+if __name__ == "__main__":
+    _run('indices_I101_1d', 1)
+    _run('indices_I101_1d', 10)
+    _run('indices_I101_1d', 50)
+    _run('indices_I101_1d', 100)
+    _run('indices_I101_1d', 200)
