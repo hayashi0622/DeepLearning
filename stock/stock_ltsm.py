659ba4a311c4ab3524e9f92d8fd0eba8abffa598
Shigeru Hayashi <hayashi@improid.net>
2017-12-25 10:50:58 +0900

    Modified a script file and Add some result figure files.

--- stock/stock_ltsm.py
+++ stock/stock_ltsm.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-import numpy
-import pandas
-import matplotlib.pyplot as plt
-
+import numpy as np
+import pandas as pd
 from sklearn import preprocessing
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
@@ -10,18 +8,18 @@ from keras.layers.recurrent import LSTM
 
 class Prediction:
 
-    def __init__(self):
-        self.length_of_sequences = 10
+    def __init__(self, length_of_sequences = 10, hidden_neurons = 300):
+        self.length_of_sequences = length_of_sequences
         self.in_out_neurons = 1
-        self.hidden_neurons = 300
+        self.hidden_neurons = hidden_neurons
 
     def load_data(self, data, n_prev=10):
         X, Y = [], []
         for i in range(len(data) - n_prev):
             X.append(data.iloc[i:(i+n_prev)].as_matrix())
             Y.append(data.iloc[i+n_prev].as_matrix())
-        retX = numpy.array(X)
-        retY = numpy.array(Y)
+        retX = np.array(X)
+        retY = np.array(Y)
         return retX, retY
 
     def create_model(self):
@@ -36,39 +34,56 @@ class Prediction:
 
     def train(self, X_train, y_train, epochs=100):
         model = self.create_model()
-        model.fit(X_train, y_train, batch_size=100, epochs=epochs)
+        model.fit(X_train, y_train, batch_size=10, epochs=epochs)
         return model
 
-def _run(file_prefix, epochs):
-    p = Prediction()
-    # データ準備
+def _run(file_prefix, length_of_sequences, hidden_neurons, epochs):
+    p = Prediction(length_of_sequences, hidden_neurons)
     data = None
-    for year in range(2007, 2017):
-        data_ = pandas.read_csv('csv/' + file_prefix + '_' + str(year) +  '.csv')
-        data = data_ if (data is None) else pandas.concat([data, data_])
+    for year in range(2007, 2018):
+        data_ = pd.read_csv('csv/{0}_{1}.csv'.format(file_prefix, year))
+        data = data_ if (data is None) else pd.concat([data, data_])
     data.columns = ['date', 'open', 'high', 'low', 'close']
-    data['date'] = pandas.to_datetime(data['date'], format='%Y-%m-%d')
-    # 終値のデータを標準化
+    data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d')
     data['close'] = preprocessing.scale(data['close'])
     data = data.sort_values(by='date')
     data = data.reset_index(drop=True)
     data = data.loc[:, ['date', 'close']]
-    # 2割をテストデータへ
-    split_pos = int(len(data) * 0.8)
+    split_pos = int(len(data) * 0.9)
     x_train, y_train = p.load_data(data[['close']].iloc[0:split_pos], p.length_of_sequences)
     x_test,  y_test  = p.load_data(data[['close']].iloc[split_pos:], p.length_of_sequences)
+    dates = data[['date']].iloc[split_pos + p.length_of_sequences:]
     model = p.train(x_train, y_train, epochs)
     predicted = model.predict(x_test)
-    result = pandas.DataFrame(predicted)
+    result = pd.DataFrame(predicted, index=dates['date'])
     result.columns = ['predict']
     result['actual'] = y_test
-    plot = result.plot(title='Stock prices prediction (epoch: {0})'.format(epochs))
+    plot = result.plot(
+            title='Nikkei Stock Average <epoch={0}>'.format(epochs),
+            figsize=(8, 5),
+            grid=True
+        )
     fig = plot.get_figure()
-    fig.savefig('epoch_{0:03d}.png'.format(epochs))
+    fig.savefig('fig/{0}_los{1:02d}_hn{2:03d}_ep{3:03d}.png'.format(
+            file_prefix, length_of_sequences, hidden_neurons, epochs))
 
 if __name__ == "__main__":
-    _run('indices_I101_1d', 1)
-    _run('indices_I101_1d', 10)
-    _run('indices_I101_1d', 50)
-    _run('indices_I101_1d', 100)
-    _run('indices_I101_1d', 200)
+    for s in [10, 20, 30]:
+        for n in [100, 300, 500]:
+            for e in [1, 10, 100]:
+                _run(file_prefix = 'indices_I101_1d', \
+                     length_of_sequences = s, \
+                     hidden_neurons = n, \
+                     epochs = e
+                    )
+    '''
+    _run(file_prefix = 'indices_I101_1d', length_of_sequences = 10, hidden_neurons = 100, epochs = 1)
+    _run(file_prefix = 'indices_I101_1d', length_of_sequences = 10, hidden_neurons = 300, epochs = 1)
+    _run(file_prefix = 'indices_I101_1d', length_of_sequences = 10, hidden_neurons = 500, epochs = 1)
+    _run(file_prefix = 'indices_I101_1d', length_of_sequences = 20, hidden_neurons = 100, epochs = 1)
+    _run(file_prefix = 'indices_I101_1d', length_of_sequences = 20, hidden_neurons = 300, epochs = 1)
+    _run(file_prefix = 'indices_I101_1d', length_of_sequences = 20, hidden_neurons = 500, epochs = 1)
+    _run(file_prefix = 'indices_I101_1d', length_of_sequences = 30, hidden_neurons = 100, epochs = 1)
+    _run(file_prefix = 'indices_I101_1d', length_of_sequences = 30, hidden_neurons = 300, epochs = 1)
+    _run(file_prefix = 'indices_I101_1d', length_of_sequences = 30, hidden_neurons = 500, epochs = 1)
+    '''
